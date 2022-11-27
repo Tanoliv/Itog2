011d87413dde72274df428666b99e207064e9398
Oleg <tmr777@mail.ru>
2022-11-27 17:56:00 +0300

    Добавил: ввод массива и вывод его в консоль

--- Itogovaja/Program.cs
+++ Itogovaja/Program.cs
@@ -1,2 +1,16 @@
 ﻿//Программа из имеющегося массива строк формирует  массив из строк,
 //длинна которых меньше или равна 3 символа.
+
+string[] array = {"слово", "пёс", "вой", "крест", "бак", "сук", "троя", "дык", "прыг", "да", "а", " "};
+
+FirstArray(array);
+
+void FirstArray(string[] array)
+{
+  for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"  [{ array[i]}]");        
+    } 
+    Console.WriteLine();
+}
+
